b61bd62a95ce3a2396dea4b61980a140a9909bea
nuwan <nuwandarshana2012@gmail.com>
2022-08-04 09:35:33 +0530

    Formated b'day using moment liberary /

--- src/Componants/ListItems/ListItems.jsx
+++ src/Componants/ListItems/ListItems.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import moment from "moment";
+
+
 function ListItems(props) {
     return (
         <React.Fragment>
-             <li> {props.NO} Name = {props.name} Course = {props.Course} </li>
+            <p>
+                <img src={props.avater} /> {props.name}</p>
+            <p> e-mail: {props.email}</p>
+            <p> City: {props.city}</p>
+
+            {/* // formating date to british pattern method one */}
+            {/* <p> Birthday: {' '}
+                {new Intl.DateTimeFormat('en-GB').format(new Date(props.birthday))}</p>   */}
+
+            {/* // formating date to british pattern using javascript liberary method two (Get  library - npm install moment) */}
+            <p> Birthday: {moment(props.birthday).format('DD-MM-YYYY')}</p>  
         </React.Fragment>
     )
 }
--- src/Componants/NameList/NameList.jsx
+++ src/Componants/NameList/NameList.jsx
@@ -3,14 +3,41 @@ import ListItems from "../ListItems/ListItems";
 
 
 function NameList() {
+
+  const nameList = {
+    "name": {
+      "title": 'Mr',
+      "first": 'Jack',
+      "last": 'Fernando'
+    },
+    "location": {
+      city: 'Hemmathagama'
+    },
+    "email": "jennie.nichols@example.com",
+    "dob": {
+      "date": "1992-03-08T15:13:16.688Z",
+      "age": 30
+    },
+    "picture": {
+      "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+    },
+
+
+
+  };
+
   return (
     <React.Fragment>
       <h1>Course participants Name List</h1>
         <ul>
 
-          <ListItems  
-          name="Nuwan Madusanka" 
-          Course = ".net"
+          <ListItems
+          // new style of string literals formatting strings  by  tilda = ``
+          name={`${nameList.name.title} ${nameList.name.first} ${nameList.name.last}`} 
+          city={nameList.location.city}
+          email={nameList.email}
+          birthday={nameList.dob.date}
+          avater= {nameList.picture.medium}
           />
 
           {/* <ListItems  name="Udara Maduranga" Course = "React"/>
